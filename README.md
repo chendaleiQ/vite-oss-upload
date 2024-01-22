1f77de20e210ce5b556666e9de099c19a0b02544
陈大雷 <chenzhongqing0724@gmail.com>
2024-01-22 13:29:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,65 @@
+vite-oss-upload
+=======
+
+将项目中打包后生产文件上传到 Ali OSS
+
+# 功能特性
+
+效果预览：
+
+# 安装
+
+```bash
+npm i -D vite-oss-upload@latest
+
+```
+
+# 基本使用
+
+在 vite.config.js 中注册本插件
+
+
+```javascript
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import viteOssUpload from 'vite-oss-upload'
+
+const options = {
+  region: '<Your Region>',
+  accessKeyId: '<Your Access Key ID>',
+  accessKeySecret: '<Your Access Key Secret>',
+  bucket: '<Your Bucket>'
+}
+
+const prod = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+  plugins: [vue(), vitePluginAliOss(options)]
+})
+```
+
+
+打包发布生产代码
+
+```
+npm run build
+```
+
+插件将会在打包完成后，上传 vite 配置 outDir 路径下的所有资源文件。
+
+# 配置项
+
+| options         | description                                             | type    | default       |
+|-----------------|---------------------------------------------------------|---------|---------------|
+| dist            | 需要上传到oss上的给定文件目录                                          | string  |               |
+| region          | 阿里云 oss 地域                                           | string  |               |
+| region          | 阿里云 oss 地域                                           | string  |               |
+| accessKeyId     | 阿里云 oss 访问 ID                                        | string  |               |
+| accessKeySecret | 阿里云 oss 访问密钥                                       | string  |               |
+| bucket          | 阿里云 oss 存储空间名称                                    | string  |               |
+| overwrite       | 如果文件已存在，是否覆盖                                    | boolean | false         |
+| ignore          | 文件忽略规则。如果你使用空字符串 `''`，将不会忽略任何文件 | boolean | `'**/*.html'` |
+| headers         | 请求头设置，详细信息请见 https://www.alibabacloud.com/help/zh/doc-detail/31978.html | object| {} |
+| test            | 仅测试路径，不会有文件上传                                  | boolean | false         |
+| enabled         | 是否启用本插件                                            | boolean | true          |
+| ...             | 其他初始化 oss 的参数，详细信息请见 https://www.alibabacloud.com/help/zh/doc-detail/64097.html | any | |
---
+++ index.js
@@ -0,0 +1,112 @@
+import color from 'picocolors'
+import { globSync } from 'glob'
+import path from 'path'
+import OSS from 'ali-oss'
+
+import { normalizePath } from 'vite'
+
+const handleIgnore = (ignore, ssrServer, ssrClient) => {
+  if (ignore === undefined) return ''
+  if (ignore) {
+    if (ssrClient) {
+      return ['**/ssr-manifest.json', '**/*.html', ...ignore]
+    }
+    if (ssrServer) {
+      return ['**']
+    }
+  }
+}
+
+export default function vitePluginAliOss(options) {
+  let baseConfig = '/'
+  let buildConfig = {}
+  baseConfig = options.dist
+
+  if (options.enabled !== undefined && !options.enabled) {
+    return
+  }
+
+  return {
+    name: 'vite-plugin-ali-oss',
+    enforce: 'post',
+    apply: 'build',
+    configResolved(config) {
+      buildConfig = config.build
+    },
+
+    closeBundle: {
+      sequential: true,
+      order: 'post',
+      async handler() {
+        const outDirPath = normalizePath(path.resolve(normalizePath(buildConfig.outDir)))
+
+        const createOssOption = Object.assign({}, options)
+        delete createOssOption.overwrite
+        delete createOssOption.ignore
+        delete createOssOption.headers
+        delete createOssOption.test
+        delete createOssOption.enabled
+
+        const client = new OSS(createOssOption)
+        const ssrClient = buildConfig.ssrManifest
+        const ssrServer = buildConfig.ssr
+
+        const files = globSync(`${outDirPath}/**/*`, {
+          nodir: true,
+          dot: true,
+          ignore: handleIgnore(options.ignore, ssrServer, ssrClient)
+        })
+
+        console.log('')
+        console.log(
+          `ali oss upload start${ssrClient ? ' (ssr client)' : ssrServer ? ' (ssr server)' : ''}`
+        )
+        console.log('')
+
+        const startTime = new Date().getTime()
+
+        for (const fileFullPath of files) {
+          const filePath = normalizePath(fileFullPath).split(`${outDirPath}/`)[1]
+
+          const ossFilePath = baseConfig + filePath
+
+          const completePath = ossFilePath
+
+          const output = `${buildConfig.outDir + filePath} => ${color.green(completePath)}`
+
+          if (options.test) {
+            console.log(`test upload path: ${output}`)
+            continue
+          }
+
+          if (options.overwrite) {
+            await client.put(ossFilePath, fileFullPath, {
+              headers: options.headers || {}
+            })
+            console.log(`upload complete: ${output}`)
+          } else {
+            try {
+              await client.head(ossFilePath)
+              console.log(`${color.gray('files exists')}: ${output}`)
+            } catch (error) {
+              if (error.code === 'NoSuchKey') {
+                await client.put(ossFilePath, fileFullPath, {
+                  headers: Object.assign(options.headers || {}, { 'x-oss-forbid-overwrite': true })
+                })
+                console.log(`upload complete: ${output}`)
+              } else {
+                throw new Error(error)
+              }
+            }
+          }
+        }
+
+        const duration = (new Date().getTime() - startTime) / 1000
+
+        console.log('')
+        console.log(color.green(`ali oss upload complete ^_^, cost ${duration.toFixed(2)}s`))
+        console.log('')
+      }
+    }
+  }
+}
